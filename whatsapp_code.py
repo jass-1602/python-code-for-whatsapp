2ecbe3179420e3627a863a7dac98136b5e99dd8a
jass-1602 <99533669+jass-1602@users.noreply.github.com>
2022-02-12 15:39:17 +0530

    Create whatsapp_code.py

---
+++ whatsapp_code.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
+# Replace below path with the absolute path
+browser = webdriver.Firefox()
+
+phone = ["78873405","+917888739405","+917888739405"]
+message = "questo è il messaggio"
+
+for i in phone:
+    url = "https://web.whatsapp.com/send?phone="+ i + "&text=" + message + "&app_absent=1"
+    time.sleep(5)
+
+    # Load Whatsapp Web page
+    browser.get(url)
+    # time.sleep(5)
+
+    # Wait for the page be loaded
+    time.sleep(10)
+
+    enter_action = ActionChains(browser)
+    time.sleep(5)
+    enter_action.send_keys(Keys.ENTER)
+    time.sleep(5)
+
+# Send message
+    enter_action.perform()
+    time.sleep(5)
+
+# Close browser
+browser.close()
